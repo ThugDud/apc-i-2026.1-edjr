63da67a7916f1e6f3ac7dbf0eebbc76413d372bb
ThugDud <ejr78763@gmail.com>
2026-03-04 10:53:31 -0300

    adiciona a primeira aula

---
+++ aulas/aula01/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+    printf("salve mundi!\n"); sleep(2);
+    printf("programmed to speak and not to feel\n"); sleep(2);
+    printf("not even sure that this is real\n"); sleep(2);
+    printf("salve mundi!\n"); sleep(2);
+    printf("find my voice\n"); sleep(2);
+    printf("althought it sounds like\n"); sleep(2);
+    printf("bits and bytes\n"); sleep(2);
+    printf("salve mundi!\n"); sleep(2);
+    return 0;
+
+}
