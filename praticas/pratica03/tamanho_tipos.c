941cb3644272b1d32929ea34fd2dd7cbbdb9d11a
ThugDud <ejr78763@gmail.com>
2026-03-31 15:01:34 -0300

    feat: conclui pratica03. Fecha #7

---
+++ praticas/pratica03/tamanho_tipos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("\ntamanho em bytes do tipo char: %d", sizeof(char));
+	printf("\ntamanho em bytes do tipo short int: %d", sizeof(short int));
+	printf("\ntamanho em bytes do tipo int: %d", sizeof(int));
+	printf("\ntamanho em bytes do tipo long int: %d", sizeof(long int));
+	printf("\ntamanho em bytes do tipo long long int: %d", sizeof(long long int));
+	printf("\ntamanho em bytes do tipo float: %d", sizeof(float));
+	printf("\ntamanho em bytes do tipo double: %d", sizeof(double));
+	printf("\ntamanho em bytes do tipo long double: %d", sizeof(long double));
+
+	return 0;
+}
