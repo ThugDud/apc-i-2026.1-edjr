e71edb3b60eaf3efe2b9122c2f2c085824bde254
ThugDud <ejr78763@gmail.com>
2026-03-30 22:40:15 -0300

    feat: parte 1 da pratica03

---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+	int idade = 20;
+	float altura = 1.75F, peso = 72.845F;
+	char sexo = 'M';	
+
+	printf("\n\nSua idade é \"%d\", é do sexo \"%c\", possuí \"%.2f\" metros de altura e pesa \"%.3f\" kilos.",idade,sexo,altura,peso);
+
+	return 0;
+}
