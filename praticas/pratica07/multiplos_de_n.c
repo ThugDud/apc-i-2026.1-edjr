d1327be95d811f3fcd80002dc11f369f3b73dac2
ThugDud <ejr78763@gmail.com>
2026-05-20 08:45:10 -0300

    corrige funcionalidade errada de multiplos_de_n

--- praticas/pratica07/multiplos_de_n.c
+++ praticas/pratica07/multiplos_de_n.c
@@ -3,11 +3,13 @@
 int main() {
 
     unsigned long long int elemento;
-    printf("Escreva o elemento a ver seus multiplos: ");
+    printf("Escreva o elemento a ver seus multiplos (1 a 100): ");
     scanf("%lu", &elemento);
 
-    for (int i = 1; i <= 100; i++) {
-        printf("\n%lu * %i = %lu", elemento, i, elemento * i);
+    for (int i = 1; i <= elemento; i++) {
+        if (elemento % i == 0){
+            printf("\n%i",i);
+        }
     }
 
     return 0;
